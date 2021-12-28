e9d34780f99f199da7922120774ac30405ef3e7b
Ninh <nguyenhuuninhneu@gmail.com>
2021-12-28 15:53:07 +0700

    remove library and owl to slick

--- js/main.js
+++ js/main.js
@@ -26,33 +26,87 @@ function orichiFormatTime(number) {
   if (number < 10) return "0" + number;
   return number;
 }
+/*Hàm cho Owlcarousel*/
 
 $(document).ready(function () {
+
   $("img").lazy({
     effect: "fadeIn",
     effectTime: 1000
   });
-  $('#slider .slider-view').owlCarousel(owlslide($('#slider .slider-view .orichi-product-item-slider').size(), 30, false, true, true, 1, 2, 2, 4, 4, "fadeIn", "fadeOut"));
+
 })
-$('.btn-view-more').on('click', function () {
-  $('.btn-view-more').css('visibility', 'hidden');
-  $('#spin-loading').css('visibility', 'visible');
+
+if ($('#orichi-layout').val() === 'slider') {
+  $('.orichi-product-list')
+    .on('init', function (slick) {
+      $('.orichi-slick-prev').addClass('orichi-hidden');
+    }).slick({
+      dots: false,
+      infinite: false,
+      lazyLoad: 'ondemand',
+      speed: 300,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      prevArrow: '<div class="orichi-slick-prev"></div>',
+      nextArrow: '<div class="orichi-slick-next"></div>',
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
+  $('.orichi-product-list').on('afterChange', function (event, slick, currentSlide) {
+    if (slick.currentSlide >= slick.slideCount - slick.options.slidesToShow) {
+      $('.orichi-slick-next').addClass('orichi-hidden');
+    }
+    else {
+      $('.orichi-slick-next').removeClass('orichi-hidden');
+    }
+
+    if (currentSlide === 0) {
+      $('.orichi-slick-prev').addClass('orichi-hidden');
+    }
+    else {
+      $('.orichi-slick-prev').removeClass('orichi-hidden');
+    }
+  })
+}
+
+
+$('.orichi-btn-view-more').on('click', function () {
+  $('.orichi-btn-view-more').css('visibility', 'hidden');
+  $('#orichi-spin-loading').css('visibility', 'visible');
   var html = '<div class="orichi-product-item"><div class="wImage"><a href="#" title="" class="image cover">  <img data-src="img/1.png" /></a></div><div class="orichi-price-wrapper"><div class="orichi-price">$150.000  <div class="orichi-discount-price">-10%</div></div></div><div class="orichi-progress-bar"><div class="flash-sale-progress-bar flash-sale-progress-bar--landing-page">  <div    class="flash-sale-progress-bar__complement-wrapper flash-sale-progress-bar__complement-wrapper--landing-page">  </div>  <div class="flash-sale-progress-bar__text">Sold 159</div>  <div class="flash-sale-progress-bar__fire">    <img src="./img/f5233259f90b05efbf3867f564628853.png" alt="">  </div></div></div>        </div>        <div class="orichi-product-item"><div class="wImage"><a href="#" title="" class="image cover">  <img data-src="img/2.jpg" /></a></div><div class="orichi-price-wrapper"><div class="orichi-price">$150.000  <div class="orichi-discount-price">-10%</div></div></div><div class="orichi-progress-bar"><div class="flash-sale-progress-bar flash-sale-progress-bar--landing-page">  <div    class="flash-sale-progress-bar__complement-wrapper flash-sale-progress-bar__complement-wrapper--landing-page">  </div>  <div class="flash-sale-progress-bar__text">Sold 159</div>  <div class="flash-sale-progress-bar__fire">    <img src="./img/f5233259f90b05efbf3867f564628853.png" alt="">  </div></div></div>        </div>        <div class="orichi-product-item"><div class="wImage"><a href="#" title="" class="image cover">  <img data-src="" /></a></div><div class="orichi-price-wrapper"><div class="orichi-price">$150.000  <div class="orichi-discount-price">-10%</div></div></div><div class="orichi-progress-bar"><div class="flash-sale-progress-bar flash-sale-progress-bar--landing-page">  <div    class="flash-sale-progress-bar__complement-wrapper flash-sale-progress-bar__complement-wrapper--landing-page">  </div>  <div class="flash-sale-progress-bar__text">Sold 159</div>  <div class="flash-sale-progress-bar__fire">    <img src="./img/f5233259f90b05efbf3867f564628853.png" alt="">  </div></div></div>        </div>';
   html += '</div>';
-  setTimeout(
-    function functionName() {
-      $('.orichi-product-list').find('.cb').remove();
-      for (let index = 0; index < 5; index++) {
-        $('.orichi-product-list').append(html);
-      }
-      $('.orichi-product-list').append('<div class="cb"></div>');
-      $("img").lazy({
-        effect: "fadeIn",
-        effectTime: 1000
-      });
-      $('.btn-view-more').css('visibility', 'visible');
-      $('#spin-loading').css('visibility', 'hidden');
-      $('.orichi-product-list').find('cb').remove();
-    }, 3000)
+  $('.orichi-product-list').find('.orichi-cb').remove();
+  $('.orichi-product-list').append(html);
+
+  $('.orichi-product-list').append('<div class="orichi-cb"></div>');
+  $("img").lazy({
+    effect: "fadeIn",
+    effectTime: 1000
+  });
+  $('.orichi-btn-view-more').css('visibility', 'visible');
+  $('#orichi-spin-loading').css('visibility', 'hidden');
+  $('.orichi-product-list').find('orichi-cb').remove();
 
 })
